f09a2572dc410144a1f7ee3516ae0834a0263832
kernel_panic <kernel_panic@34ec630f-655c-4738-8800-74af9c3ddee0>
2010-06-29 17:01:28 +0000

    Adding real UserAgent and new http headers VLM_PROXY_AGENT, VLM_PROXY_PASS, VLM_CLIENT_IP, VLM_CLIENT_FULLIP in boatsetup.class.php

--- MobiVLM/trunk/classes/boatsetup.class.php
+++ MobiVLM/trunk/classes/boatsetup.class.php
@@ -1,13 +1,14 @@
 <?php
-// PROXYID => ,"proxyid":{"ip":"'.$ip.'","proxyagent":"'.$proxyagent.'","proxypass":"'.$proxypass.'"}
+//CLASSE DE MISE A JOUR DES ORDRES DE NAVIGATION
 class boatsetup { 
 	
 function majboat($pseudo,$password,$vars,$IDU,$serveur)
 	{
 	$mode_debug = "false";
 	$ip = $this->getip();
-	$proxypass = $this->getfullip();
-	$proxyagent = $_SERVER['HTTP_USER_AGENT'];
+	$fullip = $this->getfullip();
+	$proxypass = "proxypass";
+	$userAgent = $_SERVER['HTTP_USER_AGENT'];
 	
 	switch ($vars['event'])
 		{
@@ -77,9 +78,15 @@ function majboat($pseudo,$password,$vars,$IDU,$serveur)
 		// DEBUG
 		//echo "ordre = ".$parms." pip = ".$pip." - ".$pip[0]." - ".$pip[1]." - ".$pip[2]."<hr>";
 		
-	$this->ch = curl_init('http://'.$pseudo.':'.$password.'@'.$serveur.'.virtual-loup-de-mer.org/ws/boatsetup/'.$script);
+	$this->ch = curl_init('http://'.$serveur.'.virtual-loup-de-mer.org/ws/boatsetup/'.$script);
+	curl_setopt($this->ch, CURLOPT_USERPWD, "$pseudo:$password");
+	curl_setopt($this->ch, CURLOPT_HTTPHEADER, array("VLM_PROXY_AGENT: MobiVLM"));
+	curl_setopt($this->ch, CURLOPT_HTTPHEADER, array("VLM_PROXY_PASS: ".$proxypass));
+	curl_setopt($this->ch, CURLOPT_HTTPHEADER, array("VLM_CLIENT_IP: ".$ip));
+	curl_setopt($this->ch, CURLOPT_HTTPHEADER, array("VLM_CLIENT_FULLIP: ".$fullip));
+	curl_setopt($this->ch, CURLOPT_USERAGENT, $userAgent);
+	curl_setopt($this->ch, CURLOPT_HEADER, FALSE);
 	curl_setopt($this->ch, CURLOPT_POST, TRUE);
-	//curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
 	curl_setopt($this->ch, CURLOPT_POSTFIELDS,
 	    array(
 	        'parms' => $parms
@@ -87,6 +94,7 @@ function majboat($pseudo,$password,$vars,$IDU,$serveur)
 	);
 	curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, TRUE);
 	curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, TRUE);
+	
 	$this->ret = curl_exec($this->ch);
 	if ($this->ret === FALSE) {
 	    die(curl_error());
@@ -163,5 +171,5 @@ function majboat($pseudo,$password,$vars,$IDU,$serveur)
 		return $ipstr;
 	}
 
-} // FIN DE CLASSE
+}
 ?>
