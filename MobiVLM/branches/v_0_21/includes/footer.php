421080bf47390afdfa0af47c6fd5f2082f75898c
kernel_panic <kernel_panic@34ec630f-655c-4738-8800-74af9c3ddee0>
2010-12-10 13:23:58 +0000

    add in branches old version of MobiVLM

---
+++ MobiVLM/branches/v_0_21/includes/footer.php
@@ -0,0 +1,4 @@
+<br>
+	<input name="actualiser" type="button" onclick="javascript:document.location='index.php';" value="Actualiser" class="txtbold1" />
+	<input name="logout" type="button" onclick="javascript:document.location='logout.php';" value="Logout" class="txtbold1" />
+<br>
